94ee47a5ae3798c4a6df4e4c84536ee616106b04
Hogg <joshleonard98@outlook.com>
2021-05-05 01:58:52 +0100

    added variable port

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use('/', require('./routes/pages'));
 
 
-app.listen(7000, () => {
+port = process.env.port || 3000
+app.listen(port, () => {
   console.log("Network node started");
 })
